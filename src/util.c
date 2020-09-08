64047cf503347bc9f674a41517f329130380f2a7
Siguza <siguza@siguza.net>
2020-09-09 00:47:48 +0200

    Ugh, copyright notice

--- src/util.c
+++ src/util.c
@@ -1,3 +1,13 @@
+/* Copyright (c) 2018-2020 Siguza
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This Source Code Form is "Incompatible With Secondary Licenses", as
+ * defined by the Mozilla Public License, v. 2.0.
+**/
+
 #include <fcntl.h>              // open
 #include <stdbool.h>
 #include <unistd.h>             // close
